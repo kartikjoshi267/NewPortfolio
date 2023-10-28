7ed41a330c29b167e829104666da96081c0c0dbf
Kartik <kartikjoshi267@gmail.com>
2023-10-28 17:02:51 +0530

    Minor color bug

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SectionHeading = ({ text }) => {
   return (
-    <h4 className="z-10 sticky top-0 py-5 my-0 bg-[#104420]  lg:hidden w-full text-white font-bold text-4xl">
+    <h4 className="z-10 sticky top-0 py-5 my-0 bg-[#000d2b]  lg:hidden w-full text-white font-bold text-4xl">
       {text}
     </h4>
   )
