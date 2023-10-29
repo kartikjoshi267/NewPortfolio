275251a8b9ee28b081d52b68d75011e94bae2d52
Kartik <kartikjoshi267@gmail.com>
2023-10-29 11:53:56 +0530

    Improve project card width responsiveness

--- src/components/card/ProjectsCard.jsx
+++ src/components/card/ProjectsCard.jsx
@@ -6,7 +6,7 @@ import Tags from '../miscellaneous/Tags'
 const ProjectsCard = ({ project }) => {
   return (
     <a href={project.githubRepo} target='_blank' className='group/project-card flex flex-col-reverse lg:flex-row gap-5 items-stretch lg:items-start justify-stretch lg:justify-between h-max w-[100%] duration-150 bg-transparent lg:hover:bg-[#2231a35d] hover:!opacity-100 lg:group-hover:opacity-50 p-7 rounded-lg'>
-      <img src={project.image} className='w-3/4 lg:w-1/4 h-[125px] lg:h-[75px] rounded-lg border-2 border-transparent lg:group-hover/project-card:border-gray-400 duration-150' />
+      <img src={project.image} className='w-3/4 max-w-[250px] lg:min-w-[120px] lg:w-1/4 h-[125px] lg:h-[75px] rounded-lg border-2 border-transparent lg:group-hover/project-card:border-gray-400 duration-150' />
       <div className='flex flex-col gap-5 lg:w-3/4'>
         <h4 className='text-3xl font-semibold lg:group-hover/project-card:text-[#3aebff] duration-150'>
           {project.name}
